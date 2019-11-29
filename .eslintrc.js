802983d439286bcb3c23484ba2968d25b37c3929
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-29 12:05:56 -0300

    created structure, configs and eslint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
     jest: true,
+    browser: true
   },
-  extends: [
-    'react-app',
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ["airbnb", "prettier", "prettier/react"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: true
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['react', 'import', 'jsx-a11y'],
+  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.tsx'],
-      },
-    ],
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off'
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {},
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      },
     },
   },
 };
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return (
-    <h1>Oi</h1>
-  );
-}
-
-export default App;
