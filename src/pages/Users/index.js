49c03fff09c222015b436768936f24be3f75548b
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-29 21:06:11 -0300

    added react-virtualized for better performance

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { List } from 'react-virtualized';
 
 // import { Container } from './styles';
-
-import UsersList from '~/components/UsersList';
-import Pagination from '~/components/Pagination';
+import './styles.css';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
   const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [usersPerPage] = useState(10);
+  const [age, setAge] = useState();
 
   useEffect(() => {
     async function loadUsers() {
-      setLoading(true);
       const response = await axios.get(
         'https://random-persons.herokuapp.com/users'
       );
@@ -26,32 +21,52 @@ export default function Users() {
         ...user,
       }));
       setUsers(data);
-      setLoading(false);
     }
     loadUsers();
   }, []);
 
-  const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = indexOfLastUser - usersPerPage;
-  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
-
-  function paginate(pageNumber) {
-    setCurrentPage(pageNumber);
+  function renderRow({ index, key, style }) {
+    return (
+      <div key={key} style={style} className="row">
+        <div className="content">
+          <div>Nome: {users[index].name}</div>
+          <div>Idade: {users[index].age}</div>
+        </div>
+      </div>
+    );
   }
 
-  console.tron.log(users);
+  function handleSearchByName(e) {
+    const searchName = e.target.value;
+    setName(searchName);
+
+    const data = users.filter(user => user.name === searchName);
+    setUsers(data);
+  }
+
+  function handleSearchByAge(e) {
+    const searchAge = e.target.value;
+    setAge(searchAge);
+
+    const data = users.filter(user => user.age === searchAge);
+    setUsers(data);
+  }
 
   return (
-    <div>
-      <h1>Usuários</h1>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      <input type="number" value={age} onChange={e => setAge(e.target.value)} />
-      <UsersList users={currentUsers} loading={loading} />
-      <Pagination
-        usersPerPage={usersPerPage}
-        totalUsers={users.length}
-        paginate={paginate}
-      />
-    </div>
+    <>
+      <label htmlFor="name">Nome</label>
+      <input type="text" id="name" value={name} onChange={handleSearchByName} />
+      <label htmlFor="age">Idade</label>
+      <input type="number" id="age" value={age} onChange={handleSearchByAge} />
+      <div className="list">
+        <List
+          width={800}
+          height={600}
+          rowHeight={50}
+          rowRenderer={renderRow}
+          rowCount={users.length}
+        />
+      </div>
+    </>
   );
 }
